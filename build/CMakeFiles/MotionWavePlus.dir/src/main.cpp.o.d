717ced2783f4839c9d068f9a55566f756ae2f647
Fuaad Shurie <fbshurie@hotmail.com>
2026-05-07 17:26:18 +0400

    starting multithreading

--- build/CMakeFiles/MotionWavePlus.dir/src/main.cpp.o.d
+++ build/CMakeFiles/MotionWavePlus.dir/src/main.cpp.o.d
@@ -1032,4 +1032,15 @@ CMakeFiles/MotionWavePlus.dir/src/main.cpp.o: \
   /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/unistd.h \
   /Users/fuaadshurie/Desktop/Motionwaveplus/src/render/Renderer.h \
   /Users/fuaadshurie/Desktop/Motionwaveplus/src/audio/AudioData.h \
-  /Users/fuaadshurie/Desktop/Motionwaveplus/src/audio/RingBuffer.h
+  /Users/fuaadshurie/Desktop/Motionwaveplus/src/audio/RingBuffer.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/thread \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__thread/this_thread.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__thread/thread.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/sstream \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__thread/jthread.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__stop_token/stop_source.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__stop_token/intrusive_shared_ptr.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__stop_token/stop_state.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__stop_token/atomic_unique_lock.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__stop_token/intrusive_list_view.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__stop_token/stop_token.h
--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,43 @@
 #include <GL/glew.h>
 #include <iostream>
 #include "render/Renderer.h"
-#include "audio/AudioData.h" 
+#include "audio/AudioData.h"
 #include <mutex>
+#include <thread>
 
-
-void decoderThread (AudioData * audio_data){
+void decoderThread(AudioData *audio_data)
+{
     float data[4096];
     size_t sample_size = sizeof(audio_data->audio_samples) / sizeof(audio_data->audio_samples[0]);
-    Sint16* buff = (Sint16*)(audio_data->buf);
-    
-    int current_sample_pos = 0;
-    for (int i = 0; current_sample_pos < (audio_data->len / sizeof(Sint16)); ++i){
+    Sint16 *buff = (Sint16 *)(audio_data->buf);
 
-        for (int j = 0; j < sample_size; ++j){
-            data[j] = (buff[current_sample_pos+j] / 32768.0f);
+    int current_sample_pos = 0;
+    for (int i = 0; current_sample_pos < (audio_data->len / sizeof(Sint16)); ++i)
+    {
+
+        for (int j = 0; j < sample_size; ++j)
+        {
+            data[j] = (buff[current_sample_pos + j] / 32768.0f);
         }
         current_sample_pos = current_sample_pos + 4096;
-        audio_data -> ring_buf.write( data, sample_size);
+        audio_data->ring_buf.write(data, sample_size);
     }
 }
 
-void audioCallback(void* userdata, Uint8* stream, int len) {
-    AudioData* audio = (AudioData*)userdata;
-    std::lock_guard<std::mutex> lock(audio->audio_mutex); 
-       
-    if (audio->pos >= audio->len) {
+void audioCallback(void *userdata, Uint8 *stream, int len)
+{
+    AudioData *audio = (AudioData *)userdata;
+    std::lock_guard<std::mutex> lock(audio->audio_mutex);
+
+    if (audio->pos >= audio->len)
+    {
         SDL_memset(stream, 0, len);
         return;
     }
-    
+
     Uint32 remaining = audio->len - audio->pos;
     Uint32 toCopy = (len > (int)remaining) ? remaining : len;
-    
+
     SDL_memcpy(stream, audio->buf + audio->pos, toCopy);
 
     // Sint16* stream16 = (Sint16*)stream;
@@ -44,7 +49,7 @@ void audioCallback(void* userdata, Uint8* stream, int len) {
     //     audio->audio_samples[i] = stream16[i]/32768.0f;
     // }  -------- OLD improved version below -------
 
-    Sint16* samples = reinterpret_cast<Sint16*>(stream);
+    Sint16 *samples = reinterpret_cast<Sint16 *>(stream);
 
     // bytes → samples (16-bit = 2 bytes per sample)
     Uint32 samples_requested = toCopy / sizeof(Sint16);
@@ -54,21 +59,24 @@ void audioCallback(void* userdata, Uint8* stream, int len) {
     Uint32 samples_to_process = (samples_requested > max_samples) ? max_samples : samples_requested;
 
     // convert to float [-1, 1]
-    for (Uint32 i = 0; i < samples_to_process; ++i) {
+    for (Uint32 i = 0; i < samples_to_process; ++i)
+    {
         audio->audio_samples[i] = samples[i] / 32768.0f;
     }
     audio->sample_count = samples_to_process;
     audio->pos += toCopy;
-    
+
     if (toCopy < (Uint32)len)
         SDL_memset(stream + toCopy, 0, len - toCopy);
-    
+
 } // i used ai to help for this function
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
     // --- SDL2 INIT ---
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+    {
         std::cout << SDL_GetError();
         return -1;
     }
@@ -78,40 +86,50 @@ int main(int argc, char* argv[]) {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
     // --- WINDOW ---
-    SDL_Window* window = SDL_CreateWindow(
+    SDL_Window *window = SDL_CreateWindow(
         "VLC_Motion",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         1280, 720,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
-    );
-    if (!window) { std::cout << SDL_GetError(); return -1; }
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    if (!window)
+    {
+        std::cout << SDL_GetError();
+        return -1;
+    }
 
     // --- OPENGL CONTEXT ---
     SDL_GLContext glContext = SDL_GL_CreateContext(window);
-    if (!glContext) { std::cout << SDL_GetError(); return -1; }
+    if (!glContext)
+    {
+        std::cout << SDL_GetError();
+        return -1;
+    }
 
     // --- GLEW ---
     GLenum err = glewInit();
-    if (err != GLEW_OK) {
+    if (err != GLEW_OK)
+    {
         std::cout << "GLEW Error: " << glewGetErrorString(err) << "\n";
         return -1;
     }
     std::cout << "GLEW: " << glewGetString(GLEW_VERSION) << "\n";
 
     // --- RENDERER ---
-   std::cout << "Starting renderer init\n";
+    std::cout << "Starting renderer init\n";
     Renderer renderer;
-    if (!renderer.init()) return -1;
-    std::cout << "Renderer init done\n";    
+    if (!renderer.init())
+        return -1;
+    std::cout << "Renderer init done\n";
 
     // --- AUDIO FILE ---
-    const char* file = "/Users/fuaadshurie/Desktop/cable-car.wav";
+    const char *file = "/Users/fuaadshurie/Desktop/cable-car.wav";
     SDL_AudioSpec spec;
-    Uint8* audio_buf;
+    Uint8 *audio_buf;
     Uint32 audio_len;
 
     SDL_LoadWAV(file, &spec, &audio_buf, &audio_len);
-    if (!audio_buf) {
+    if (!audio_buf)
+    {
         std::cout << "SDL_LoadWAV failed: " << SDL_GetError() << "\n";
         return -1;
     }
@@ -122,17 +140,18 @@ int main(int argc, char* argv[]) {
     audio_data.len = audio_len;
     audio_data.pos = 0;
     audio_data.sample_count = 0;
+    std::thread decoder(decoderThread, &audio_data);
     spec.callback = audioCallback;
     spec.userdata = &audio_data;
 
-
-    std::cout << "WAV loaded, buf=" << (void*)audio_buf << " len=" << audio_len << "\n";
+    std::cout << "WAV loaded, buf=" << (void *)audio_buf << " len=" << audio_len << "\n";
 
     SDL_AudioSpec obtained;
     int isCapture = 0;
     SDL_AudioDeviceID OpenAudioDevice = SDL_OpenAudioDevice(NULL, isCapture, &spec, &obtained, 0);
     std::cout << "Audio device opened: " << OpenAudioDevice << "\n";
-    if (OpenAudioDevice == 0) {
+    if (OpenAudioDevice == 0)
+    {
         std::cout << "SDL_OpenAudioDevice failed: " << SDL_GetError() << "\n";
         return -1;
     }
@@ -141,16 +160,20 @@ int main(int argc, char* argv[]) {
 
     // --- RENDER LOOP ---
     bool running = true;
-    while (running) {
+    while (running)
+    {
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) running = false;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                running = false;
         }
         renderer.draw(&audio_data);
         SDL_GL_SwapWindow(window);
     }
 
     // --- CLEANUP ---
+    decoder.join();
     SDL_CloseAudioDevice(OpenAudioDevice);
     SDL_FreeWAV(audio_buf);
     renderer.cleanup();
